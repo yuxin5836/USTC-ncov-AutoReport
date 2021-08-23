ce24ebf90cc7813e507ac09f6194b0e705258a10
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2021-08-23 10:03:29 +0800

    Add files via upload

--- 验证码处理研究/validate.py
+++ 验证码处理研究/validate.py
@@ -1,12 +1,16 @@
+import requests
+from io import BytesIO
 import pytesseract
 from PIL import Image, ImageDraw
 import numpy as np
 import cv2
-image=cv2.imread('t.png')
+text=requests.get('https://passport.ustc.edu.cn/validatecode.jsp?type=login',stream=True).content
+image=Image.open(BytesIO(text))
+image=cv2.cvtColor(np.asarray(image),cv2.COLOR_RGB2BGR)
 kernel = np.ones((3,3),np.uint8)
 image = cv2.dilate(image,kernel,iterations = 1)
+image = cv2.erode(image,kernel,iterations = 1)
 image = Image.fromarray(image)
 image.show()
 text=pytesseract.image_to_string(image)
-print(text)
-
+print(text[:4])
